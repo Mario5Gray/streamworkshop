54a3c61ddb0b4fe084931444dc2cfca5ca637846
Mario Gray <mario.gray@gmail.com>
2021-08-31 18:07:00 -0700

    fixes stream tst

--- streamworkshop-java/function/src/test/java/com/example/workshop/test/StockFunctionTests.java
+++ streamworkshop-java/function/src/test/java/com/example/workshop/test/StockFunctionTests.java
@@ -5,13 +5,12 @@ import com.example.workshop.domain.Stock;
 import com.example.workshop.function.StockFunctions;
 import com.example.workshop.repository.StockRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.InputDestination;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
@@ -24,11 +23,13 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
 
 import java.util.HashMap;
 
 @Testcontainers
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+@Tag("integration")
 public class StockFunctionTests {
 
     @Container
@@ -52,6 +53,15 @@ public class StockFunctionTests {
                 "--spring.cloud.stream.bindings.snapshot-in-0.destination=snapshot-req",
                 "--spring.cloud.stream.bindings.snapshot-out-0.destination=snapshots"
         )) {
+            var repository = context.getBean(StockRepository.class);
+            StepVerifier
+                    .create(repository.save(new Stock("TEST", 42.0)))
+                    .assertNext(s -> {
+                        Assertions
+                                .assertThat(s)
+                                .isNotNull();
+                    })
+                    .verifyComplete();
             var converter = context.getBean(CompositeMessageConverter.class);
             var headers = new HashMap<String, Object>();
             headers.put("contentType", "application/json");
