d6d4b154f8657620b3afbeaebf2cbd99fb94f1f1
Mario Gray <mario.gray@gmail.com>
2021-09-01 11:34:56 -0700

    polish

---
+++ streamworkshop-java/function-trade/src/main/java/com/example/workshop/function/StockFunctions.java
@@ -0,0 +1,30 @@
+package com.example.workshop.function;
+
+import com.example.workshop.domain.Stock;
+import com.example.workshop.domain.TradeRequest;
+import com.example.workshop.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class StockFunctions {
+    private final StockService stockService;
+
+    @Bean
+    public Function<Flux<TradeRequest>, Flux<Stock>> trade() {
+        return trades -> trades.flatMap(s ->
+                stockService
+                        .tradeStock(s.getSymbol(), s.getPrice())
+        );
+    }
+
+    @Bean
+    public Function<Mono<String>, Mono<Stock>> snapshot() {
+        return stringMono -> stringMono
+                .flatMap(stockService::getQuote);
+    }
+}
