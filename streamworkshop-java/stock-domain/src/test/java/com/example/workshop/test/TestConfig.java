212818291a90b3482ddd3b13b2c454cb7281751a
Mario Gray <mario.gray@gmail.com>
2021-09-02 04:51:00 -0700

    fixes

--- streamworkshop-java/stock-domain/src/test/java/com/example/workshop/test/TestConfig.java
+++ streamworkshop-java/stock-domain/src/test/java/com/example/workshop/test/TestConfig.java
@@ -1,10 +1,9 @@
 package com.example.workshop.test;
 
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-//@SpringBootConfiguration
-//@EnableReactiveMongoRepositories
+@SpringBootApplication
+@EnableReactiveMongoRepositories(basePackages = {"com.example.workshop.repository"})
 public class TestConfig {
 }
--- streamworkshop-java/stock-trades/src/main/java/com/example/workshop/function/trade/TradeFunctions.java
+++ streamworkshop-java/stock-trades/src/main/java/com/example/workshop/function/trade/TradeFunctions.java
@@ -16,7 +16,7 @@ public class TradeFunctions {
     private final StockService stockService;
 
     @Bean
-    public Function<Flux<TradeRequest>, Flux<Stock>> conflate() {
+    public Function<Flux<TradeRequest>, Flux<Stock>> trade() {
         return trades -> trades.flatMap(s ->
                 stockService
                         .tradeStock(s.getSymbol(), s.getPrice())
